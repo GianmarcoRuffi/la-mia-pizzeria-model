f6e8ca4b84ec952f9adf05c0758a1688e6ecbc60
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-11-21 14:28:42 +0100

    Aggiunto Pizzacontroller e connessione al db

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            //ListaPizze listaPizze = new ListaPizze("Mario", "Rossi", 25);
+
+        return View();
+        }
+    }
+}
